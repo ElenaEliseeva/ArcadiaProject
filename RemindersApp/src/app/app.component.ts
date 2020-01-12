068bd65e09770d73f4ed85262e4d56f3a8db90d0
Elena Eliseeva <e.e.a.march@gmail.com>
2020-01-12 12:16:37 +0300

    Creating a visual part

---
+++ RemindersApp/src/app/app.component.ts
@@ -0,0 +1,55 @@
+﻿import { Component } from '@angular/core';
+import { strict } from 'assert';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+class Reminder {
+    id: number;
+    body: string;
+    timeToWork: string; 
+    idPriority: number;
+    idCookie: number;
+
+
+    constructor(id: number, body: string, timeToWork: string, idPriority: number, idCookie: number) {
+        this.id =id;
+        this.body = body;
+        this.timeToWork = timeToWork; 
+        this.idPriority = idPriority;
+        this.idCookie = idCookie;
+    }
+}
+
+@Component({
+    selector: 'purchase-app',
+    templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+    appForm: FormGroup;
+    constructor() {
+        this.appForm = new FormGroup({
+
+            "remindersBody": new FormControl("Текст напоминания", Validators.required),
+            "remindersDate": new FormControl("", [Validators.required]),
+        });
+    }
+
+    reminders: Reminder[] =
+        [
+            { id: 1, body: "Купить хлеб", timeToWork: "2020", idPriority: 1, idCookie: 1 },
+            { id: 2, body: "Купить колбасу", timeToWork: "2020", idPriority: 1, idCookie: 1 },
+            { id: 3, body: "Купить слона", timeToWork: "2020", idPriority: 1, idCookie: 1 }
+        ];
+
+    addItem(body: string, date: JSON, time: JSON): void {
+
+        if (body == null || body.trim() == "")
+            return;
+
+        var timeToWork = date['year'] + "/" + date['month'] + "/" + date['day'] + " " + time['hour'] + ":" + time['hour'];
+        this.reminders.push(new Reminder(4, body, timeToWork, 1, 1));
+    }
+
+    date = { year: 2020, month: 1, day: 30};
+    time = { hour: 13, minute: 30 };
+}
---
+++ RemindersApp/src/app/app.module.ts
@@ -0,0 +1,14 @@
+﻿import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [BrowserModule, FormsModule, NgbModule, ReactiveFormsModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
